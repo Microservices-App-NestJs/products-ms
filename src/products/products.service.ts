877f7e9a875edaf74ccdb0a2d5c8ce2976e97955
samuel20468 <136033542+samuel20468@users.noreply.github.com>
2024-08-04 22:18:12 -0500

    adjustments

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient, Product } from '@prisma/client';
@@ -45,7 +45,11 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
         available: true,
       },
     });
-    if (!product) throw new RpcException(`Product with id: #${id} not found`);
+    if (!product)
+      throw new RpcException({
+        message: `Product with id: #${id} not found`,
+        statusCode: HttpStatus.BAD_REQUEST,
+      });
 
     return product;
   }
